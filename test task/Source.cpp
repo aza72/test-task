1ae29e1cf90daf14418410b98ac698e90d707db7
aza72 <mavtodru@mail.ru>
2023-07-21 12:12:54 +0500

    #3work 24.07.2023

--- test task/Source.cpp
+++ test task/Source.cpp
@@ -8,15 +8,6 @@ using namespace std;
 
 
 
-class A 
-{
-public:
-	A(int x, int y) {
-		
-	}
-};
-
-
 
 
 
@@ -27,10 +18,11 @@ int main()
 	int point=0;
 	int size = 4;
 	int arr[4][2];
+	string str;
 	string polys = "polys.txt";
 	string lines = "lines.txt";
 	ofstream fileout_polys, fileout_lines;
-
+	ifstream in(polys);
 	fileout_polys.open(polys);
 	fileout_lines.open(lines);
 
@@ -43,7 +35,7 @@ int main()
 	{
 		cout << "File opening error" << endl;
 	}
-
+	
 	
 	for (int i = 0; i < size; i++)
 	{
@@ -64,13 +56,36 @@ int main()
 
 	}
 
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 2; j++)
+		{
+			if (j==0)
+			{
+				fileout_polys << "X=" << arr[i][j]<<",";
+			}
+			if (j==1)
+			{
+				fileout_polys << "Y=" << arr[i][j]<<"\t";
+			}
+		}
 
-
-
-
-
-
+	}
 	fileout_polys.close();
+
+	while (!in.eof())
+	{
+		getline(in, str);
+		cout << str;
+	}
+	
+	in.close();
+	
+	char str2 = "=";
+	string res= str.find(str,0);
+	
+	cout << res;
+	
 	fileout_lines.close();
 
 	return 0;
