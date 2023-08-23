dd4058ebde6d93c1fb5c91fdb1e435d9e3c47ab4
aza72 <mavtodru@mail.ru>
2023-08-23 21:06:42 +0500

    home 23/08

--- opencv_project/Source.cpp
+++ opencv_project/Source.cpp
@@ -9,9 +9,24 @@ using namespace std;
 using namespace cv;
 
 
+
+
+
 int main()
 {
-	Mat img= imread("C:/Users/1/Desktop/opencv/j1.jpg");
+
+
+	Mat img = imread("C:/Users/1/Desktop/opencv/j1.jpg");
+	Mat scr = img;
+
+
+
+
+
+
+
+
+	
 	Mat grayimg, blurred, thresholded,contr;
 	if (img.empty())
 	{
@@ -20,22 +35,64 @@ int main()
 	
 	namedWindow("windowimg");
 	imshow("windowimg", img);
+    Mat graymat;
+	cvtColor(img, graymat, COLOR_BGR2GRAY);
 
-	//Mat graymat;
-	//cvtcolor(img.graymat, cv::COLOR_BGR2GRAY);
-	cvtColor(img, grayimg, COLOR_BGR2GRAY);
-	//gimg = GaussianBlur(gray, (5, 5), 0);
-	 GaussianBlur(grayimg,blurred, Size(3, 3), 0);
-	 threshold(blurred, thresholded, 100, 255, THRESH_BINARY);
-	 /*findContours(thresholded, contr, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
-	 drawContours(img, contr, -1, (0, 255, 0), 2);*/
+	Mat raz;
+	medianBlur(graymat, raz, 5);
+
+
 	 Mat edges;// = Canny(gray, 0, 50);
-	 Canny(blurred, edges, 0, 50);
+	 Canny(raz, edges,200, 200);
+     Mat g= getStructuringElement(MORPH_RECT, Size (7, 7));
+     Mat kernel;
+	 morphologyEx(edges, kernel, MORPH_CLOSE, g);
+
+
+
+	// vector< vector< cv::Point> > contours;
+
+	 vector < vector<  Point> >  contours;
+	 vector<Vec4i> hierarchy;
+	// Scalar color(rand() & 255, rand() & 255, rand() & 255);
+	 findContours(kernel, contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+	 
+	 int index = 0; 
+	
+		 drawContours(scr, contours, index, Scalar(0, 100, 100), FILLED, 0, hierarchy);
+	
+
 	namedWindow("windowimg1");
-	imshow("windowimg1", edges);
+	imshow("windowimg1", scr);
 
 	waitKey(0);
 	destroyWindow("windowimg");
 	
 	return 0;
 }
+
+
+
+
+
+//// Загрузка изображения
+//Mat image = imread("spoon.jpg");
+//
+//// Определение параметров спичечного коробка: ширина, высота и цвет
+//int width = 40; int height = 50; Vec3b color = Scalar(0, 255, 0);
+//
+//// Поиск спичечного коробка на изображении с использованием функции findContours()
+//vector<vector<Point>> contours;
+//findContours(image, contours, CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE);
+//
+//for (vector<Point> contour : contours) {
+//	// Если контур имеет форму спичечного коробка и соответствует заданным параметрам
+//	if (contourArea(contour) > 500 &&
+//		contourPerim(contour) / abs(contourArea(contour)) < 0.05 &&
+//		matchShapes(contour, Rect(0, 0, width, height), color, 1, 0.5) != -1) {
+//
+//		// Вывод координат центра спичечного коробка на экран
+//		cout << contourCenter(contour)[0] << " " << contourCenter(contour)[1] << endl;
+//	}
+//}
+
